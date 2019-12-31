d9dddbdcc75c3a94da7eeeabb52a861dc8ac8189
rajkonkret <rajkonkret660@gmail.com>
2019-12-31 22:58:14 +0100

    drobne zmiany

--- src/ErastoSive.java
+++ src/ErastoSive.java
@@ -6,8 +6,8 @@ public class ErastoSive {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         List<Boolean> number = new ArrayList<>();
-        String ans = "t";
-        while (ans.equalsIgnoreCase("t")) {
+        String answer = "t";
+        while (answer.equalsIgnoreCase("t")) {
 
             System.out.println("Podaj liczbę do której mam " +
                     "wyznaczyć liczby pierwsze: ");
@@ -19,7 +19,7 @@ public class ErastoSive {
 
             for (int i = 2; i * i <= max; i++) {
 
-                if (number.get(i) == true) {
+                if (number.get(i)) {
 
                     for (int j = 2; j * i <= max; j++) {
                         number.set(j * i, false);
@@ -30,13 +30,13 @@ public class ErastoSive {
             System.out.println("W zakresie do " + max + " liczby pierwsze to:");
 
             for (int n = 2; n < number.size(); n++) {
-                if (number.get(n) == true) {
+                if (number.get(n)) {
                     System.out.print(n + " ");
                 }
             }
             number.clear();
             System.out.println("\nJeszcze raz? (t/n)");
-            ans = scanner.next();
+            answer = scanner.next();
         }
 
         System.out.println("Koniec");
